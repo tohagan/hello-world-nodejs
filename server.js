04ad38382956440710da6a0bdd2c4f8176bc0c47
Tony OHagan <tony@ohagan.name>
2025-02-07 16:09:12 +1000

    Removed header logging

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ const path = require('path')
 const app = express()
 
 app.get('/', async (req, res) => {
-  console.log('Request Headers:', req.headers);
-
-  // Log additional meta-data about the request
-  console.log('Request Method:', req.method);
-  console.log('Request URL:', req.originalUrl);
-  console.log('Protocol:', req.protocol);
-  console.log('IP Address:', req.ip);
-  console.log('Query Parameters:', req.query);
-  console.log('Timestamp:', new Date().toISOString());
   res.sendFile(path.join(__dirname, '/index.html'));
 })
 
