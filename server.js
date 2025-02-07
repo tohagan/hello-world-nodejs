7dcdddf291818e8a616ba083a202c822af094499
Tony OHagan <tony@ohagan.name>
2025-02-07 14:42:12 +1000

    Log request details

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const path = require('path')
 const app = express()
 
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+  console.log('Request Headers:', req.headers);
+
+  // Log additional meta-data about the request
+  console.log('Request Method:', req.method);
+  console.log('Request URL:', req.originalUrl);
+  console.log('Protocol:', req.protocol);
+  console.log('IP Address:', req.ip);
+  console.log('Query Parameters:', req.query);
+  console.log('Timestamp:', new Date().toISOString());
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Hello World Application is running on port ${process.env.PORT}`)
+  console.log(`Hello World Application is running on port ${process.env.PORT}`)
 })
